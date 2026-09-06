a4da34b49116ed7cbd211c06d2cbc671105b3a1c
minseo-1129 <m.cho24@imperial.ac.uk>
2026-09-06 12:51:32 +0900

    fix: remove unused search import

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../data/menu_catalog.dart';
-import '../models/coffee_record.dart';
 import '../state/app_state.dart';
 import '../widgets/record_card.dart';
 import 'detail_screen.dart';
